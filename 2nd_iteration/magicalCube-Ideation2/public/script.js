78bc683b8cf7f37f41426e4c8778a3a3a1e74e89
Julija Rukanskaite <julijar@users.github.com>
2019-03-22 11:31:39 +0100

    dimming light features initial sketch

--- 2nd_iteration/magicalCube-Ideation2/public/script.js
+++ 2nd_iteration/magicalCube-Ideation2/public/script.js
@@ -19,14 +19,13 @@ function onData(e) {
   var accel = e.accel;
   var accelGrav = e.accelGrav;
   var rot = e.rot;
-  
- // console.log("hello its Clint's code");
+
   if (!frozen) {
-    showData(e);
-    colorTheBackground(e);
+    //showData(e);
+    changeLightAmount(e);
+    //dimTheLight(e);
   }
 }
-
 
 function initWebsocket() {
   const url = 'ws://' + location.host + '/ws';
@@ -45,69 +44,33 @@ function initWebsocket() {
   };
 }
 
-/* //window.addEventListener("deviceorientation", handleOrientation, true);
-function handleOrientation(event) {
-  if (200 > event.alpha > 129) {
-document.body.style.backgroundColor = "red";
-console.log("Hello");
-}
-};*/
-
-// greater than > , less than <
-// uppright
-/* function colorTheBackground(event){
-  if (event.rot.beta < 80 && event.rot.beta > 1) {
-    console.log("Hello bye");
-    document.body.style.backgroundColor = "red";
-  } else if (event.rot.beta > 81 && event.rot.beta < 170) {
-    document.body.style.backgroundColor = "blue";
-  } else if (event.rot.beta > 171 && event.rot.beta ) {
-  document.body.style.backgroundColor = "green";
-  } else if (event.rot.gamma < 70 && event.rot.beta < 1 && event.rot.beta < 170) {
-  document.body.style.backgroundColor = "purple";
-  } else if (event.rot.gamma > -70 && event.rot.beta < -1)  {
-  document.body.style.backgroundColor = "yellow";
-  }
-}; */
-
-//sideways
-
- function colorTheBackground(event){
-  if (event.rot.alpha > 0 && event.rot.alpha < 30) {
-    document.body.style.backgroundColor = "red";
-  } else if (event.rot.alpha > 30) {
-    document.body.style.backgroundColor = "blue";
-  } else if (event.rot.alpha > 50 && ) {
-  document.body.style.backgroundColor = "green";
+//sideways control of different "dimming" states of the light
+ function changeLightAmount(event){
+  if (event.rot.gamma > 0 && event.rot.gamma < 45) {
+    document.getElementById('last').style.height = "100px";
+    document.getElementById('last').style.width = "100px"
+  } else if (event.rot.gamma > 45 && event.rot.gamma < 90) {
+    document.getElementById('last').style.height = "300px";
+    document.getElementById('last').style.width = "300px"
+  } else if (event.rot.gamma > -90 && event.rot.gamma < -45) {
+    document.getElementById('last').style.height = "600px";
+    document.getElementById('last').style.width = "600px";
+  } else if (event.rot.gamma > -45 && event.rot.gamma < 0) {
+    document.getElementById('last').style.height = "600px";
+    document.getElementById('last').style.width = "900px";
   }
 }; 
 
-
-//rotation
-/* function colorTheBackground(event){
-  if (event.rot.alpha > 8 && event.rot.alpha < 45) {
-    document.body.style.backgroundColor = "red";
-  } else if (event.rot.alpha > 46 && event.rot.alpha < 50) {
-    document.body.style.backgroundColor = "blue";
-  } else if (event.rot.alpha > 329 && event.rot.alpha < 342 ) {
-  document.body.style.backgroundColor = "green";
+//dimTheLight when the object is layed down
+ /*function dimTheLight(event){
+  if (event.rot.beta > -2 && event.rot.beta < 3) {
+    document.body.style.backgroundColor = "grey";
+    document.getElementById('last').backgroundColor = "grey";
+  } else {
+    document.body.style.backgroundColor = "black";
+    document.getElementById('last').style.backgroundColor= "white";
+    document.getElementById('last').style.height = "100px";
+    document.getElementById('last').style.width = "100px"
   }
-};
- */
-// asd
-function showData(m) {
-  let html = 'accel';
-  html += '<table><tr><td>' + m.accel.x.toFixed(3) + '</td><td>' + m.accel.y.toFixed(3) + '</td><td>' + m.accel.z.toFixed(3) + '</tr></table>';
-  html += '</table>';
-  
-  html += 'rot';
-  html += '<table><tr><td>' + m.rot.alpha.toFixed(3) + '</td><td>' + m.rot.beta.toFixed(3) + '</td><td>' + m.rot.gamma.toFixed(3) + '</tr></table>';
-  
-  html += 'rotMotion';
-  html += '<table><tr><td>' + m.rotMotion.alpha.toFixed(3) + '</td><td>' + m.rotMotion.beta.toFixed(3) + '</td><td>' + m.rotMotion.gamma.toFixed(3) + '</tr></table>';
-  
-  html += 'accelGrav';
-  html += '<table><tr><td>' + m.accelGrav.x.toFixed(3) + '</td><td>' + m.accelGrav.y.toFixed(3) + '</td><td>' + m.accelGrav.z.toFixed(3) + '</tr></table>';
-  html += '</table>';
-  document.getElementById('last').innerHTML = html;
-}
+ }; */
+ 
