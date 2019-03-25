5bad9778c938c7712b8db79083bdc530b472903e
Liam Aljundi <liam.aljundi@gmail.com>
2019-03-25 15:07:57 +0100

    changed dots' colors by changing their id from the DOM

--- 2nd_iteration/rotationLamp/public/script.js
+++ 2nd_iteration/rotationLamp/public/script.js
@@ -17,6 +17,31 @@ function onDocumentReady() {
         
         // Parse message, assuming <Text,Int,Float>
         var d = evt.data.trim();
+
+        if(d > 350 && d <550){
+          document.getElementById("dot1").id= "newDot1";
+        }else{
+          document.getElementById("dot1").id= "dot1";
+        };
+
+        if(d > 550 && d <750){
+          document.getElementById("dot2").id= "newDot2";
+        }else{
+          document.getElementById("dot2").id= "dot2";
+        };
+        if(d > 750 && d <950){
+          document.getElementById("dot3").id= "newDot3";
+        }else{
+          document.getElementById("dot3").id= "dot3";
+        };
+        if(d > 950 && d <1023){
+          document.getElementById("dot4").id= "newDot4";
+        }else{
+          document.getElementById("dot4").id= "dot4";
+        };
+        
+
+        console.log(d) ;
         if (d.charAt(0) == '<' && d.charAt(d.length-1) == '>') {
             // Looks legit
             d = d.split(',');    
@@ -28,6 +53,7 @@ function onDocumentReady() {
                 });
                 return;          
             }
+
         }
         
         // Doesn't seem to be formatted correctly, just display as-is
@@ -40,7 +66,7 @@ function onDocumentReady() {
         console.log("Socket opened");
     }
 
-    sendFormEl.addEventListener('sendForm', function(evt) {
+    sendFormEl.addEventListener('submit', function(evt) {
         evt.preventDefault();
         var send = document.getElementsByClassName('sendtoSerial').value;
         socket.send(send);  
@@ -49,15 +75,21 @@ function onDocumentReady() {
 
     function lightOff() {
     document.getElementsByClassName('sendtoSerial').value = '0';
+    console.log('button pressed')
   }function light1() {
     document.getElementsByClassName('sendtoSerial').value = '1';
+    console.log('button pressed')
   }
   function light2() {
     document.getElementsByClassName('sendtoSerial').value = '2';
+    console.log('button pressed')
   }
   function light3() {
     document.getElementsByClassName('sendtoSerial').value = '3';
+    console.log('button pressed')
   }
   function light4() {
     document.getElementsByClassName('sendtoSerial').value = '4';
+    console.log('button pressed')
   }
+
